72c91589f915f2f76954394d4db4dd2b074e9bef
Ebuka MartinsEchesi <martin4best@gmail.com>
2024-11-09 22:23:18 +0000

    create table using jQuery

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("th").addClass("background-color");
+    $("th").addClass("color");
+});
+
+$(document).ready(function() {
+    $("tr:odd").addClass("odd");
+    $("tr:even").addClass("even");
+});
